061ff8090dcb50de3b56abccdf5e2263e11030ae
Håkon Jordet <haakon.jordet@gmail.com>
2020-02-24 13:02:24 +0100

    Allow selection of algorithms

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use crate::module_handling::ModuleInfo;
+
 ///Create a general Redis key to be used in the system.
 #[cfg(not(test))]
 pub fn create_redis_key(name: &str) -> String {
@@ -19,4 +21,21 @@ pub fn create_redis_backend_key(name: &str) -> String {
 #[cfg(test)]
 pub fn create_redis_backend_key(name: &str) -> String {
     format!("laps.testing.backend.{}", name)
+}
+
+//Get the job queue key for `module`.
+pub fn get_module_key(module: &ModuleInfo) -> String {
+    let prefix = create_redis_key("runner");
+    format!("{}.{}:{}.work", prefix, module.name, module.version)
+}
+
+//Get the job token to job id map token key using `token`.
+pub fn get_job_mapping_key(token: &str) -> String {
+    let prefix = create_redis_backend_key("job_mapping");
+    format!("{}.{}", prefix, token)
+}
+
+pub fn get_job_key(jobid: i32) -> String {
+    let prefix = create_redis_backend_key("job_result");
+    format!("{}.{}", prefix, jobid)
 }
