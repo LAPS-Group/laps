054713c66e6d47d2052a0592300c3f1bf7d1cb30
Håkon Jordet <haakon.jordet@gmail.com>
2020-02-24 09:51:06 +0100

    Set up prettier check in travis

--- frontend/index.js
+++ frontend/index.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
-import Vue from 'vue';
+import axios from "axios";
+import Vue from "vue";
 
 var app = new Vue({
-    el: '#root',
-    data: {
-        coordinates: {
-            start: {
-                x: 1,
-                y: 2
-            },
-            end: {
-                x: 2,
-                y: 4
-            }
-        }
-    },
-    methods: {
-        submit_points: function() {
-            let message = JSON.stringify(self.coordinates);
-            console.log(message);
-            axios.post('/job/submit', message, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-        }
+  el: "#root",
+  data: {
+    coordinates: {
+      start: {
+        x: 1,
+        y: 2
+      },
+      end: {
+        x: 2,
+        y: 4
+      }
     }
+  },
+  methods: {
+    submit_points: function() {
+      let message = JSON.stringify(self.coordinates);
+      console.log(message);
+      axios.post("/job/submit", message, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    }
+  }
 });
