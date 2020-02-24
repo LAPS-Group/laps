97bd8b72b2b90182baa678ebc600b01d3427eff8
Håkon Jordet <haakon.jordet@gmail.com>
2020-02-24 13:11:33 +0100

    Run prettier

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,15 @@
-import axios from 'axios';
-import Vue from 'vue';
-import SendCords from './components/SendCords.vue';
-import App from './components/App.vue';
-
+import axios from "axios";
+import Vue from "vue";
+import SendCords from "./components/SendCords.vue";
+import App from "./components/App.vue";
 
 //test app that displays hello world
 new Vue({
-    el: '#app',
-    render: h => h(App),
+  el: "#app",
+  render: h => h(App)
 });
 //Calls send app, its sends coordinates
 new Vue({
-    el: '#sendCords',
-    render: s => s(SendCords),
-})
+  el: "#sendCords",
+  render: s => s(SendCords)
+});
