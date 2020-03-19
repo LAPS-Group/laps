8b9315b30e06e11d5cb350e1d2c0532f077fc6a5
Håkon Jordet <haakon.jordet@gmail.com>
2020-03-19 11:51:47 +0100

    Add Nix development environment script

---
+++ laps.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+  name = "laps-shell";
+  src = null;
+  buildInputs = [pkgs.gdal_2 pkgs.llvmPackages.libclang pkgs.clang pkgs.nodejs ];
+  shellHook = ''
+export LIBCLANG_PATH=${pkgs.llvmPackages.libclang}/lib
+export GDAL_INCLUDE_DIR=${pkgs.gdal_2}/include
+export GDAL_LIB_DIR=${pkgs.gdal_2}/lib
+'';
+}
