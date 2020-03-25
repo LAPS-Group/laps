5f98e96fc79240d16414d53696f4aeda36664de0
Even <eventr3@gmail.com>
2020-03-25 11:10:52 +0100

    MVP

--- frontend/store.js
+++ frontend/store.js
@@ -2,11 +2,11 @@ import Vue from "vue";
 //store variables
 export const store = Vue.observable({
   tester: 0,
-  recivedCoordinates: {
-    test: "noe"
-  }
+  recivedCoordinates: { test: "noe" },
+  selected_algorithms: [],
+  job_token: null,
+  map_id: null
 });
-
 //function to update a variable
 export const mutations = {
   settester(tester) {
@@ -14,5 +14,14 @@ export const mutations = {
   },
   setrecivedCoordinates(recivedCoordinates) {
     store.recivedCoordinates = recivedCoordinates;
+  },
+  setselected_algorithms(selected_algorithms) {
+    store.selected_algorithms = selected_algorithms;
+  },
+  setjob_token(job_token) {
+    store.job_token = job_token;
+  },
+  setmap_id(map_id) {
+    store.map_id = map_id;
   }
 };
