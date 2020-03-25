f1ce40fb17c3c06f836570b313ae11f6dc77784e
Even <eventr3@gmail.com>
2020-03-25 11:10:52 +0100

    MVP

---
+++ frontend/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+    <!--<img :src="images.Laps">-->
+  </div>
+</template>
+<script>
+//import laps_logo from "./LAPS1.png";
+/*
+function component() {
+  const element = document.createElement("div");
+  const myIcon = new Image();
+  myIcon.src = laps_logo;
+
+  element.appendChild(myIcon);
+ 
+  return element;
+}
+document.body.appendChild(component());
+ document.getElementById("myIcon").width = "300"; 
+ */
+export default {
+  data() {
+    return {
+      images: {
+        Laps: require("./LAPS1.png")
+      }
+    };
+  }
+};
+</script>
--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,9 @@ import Vue from "vue";
 //import App from './components/App.vue';
 import SendCords from "./components/SendCords.vue";
 import getMap from "./components/GetMap.vue";
-//import DrawCords from './components/DrawCords.vue';
+import getAlgorithm from "./components/GetAlgorithm.vue";
+import header from "./components/Header.vue";
+//import laps_logo from "./LAPS1.png";
 
 //Calls send app, its sends coordinates
 new Vue({
@@ -13,4 +15,12 @@ new Vue({
 new Vue({
   el: "#getMap",
   render: g => g(getMap)
+});
+new Vue({
+  el: "#getAlgorithm",
+  render: a => a(getAlgorithm)
+});
+new Vue({
+  el: "#header",
+  render: h => h(header)
 });
